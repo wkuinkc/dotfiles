e5d7f257c38b74fdbaaac02c5b68f6e50bfee8c8
wkuinkc <wkuinkc@gmail.com>
2019-03-08 23:18:20 +0000

    Added a parameter to stow.sh for the target directory

--- stow.sh
+++ stow.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "$1" = "" ]
+then
+  echo "usage: stow.sh target-directory" >&2
+  exit 1
+fi
+TARGET="$1"
 pushd "$DIR"
-stow -t "$HOME"/ -S root
+stow -t "$TARGET"/ -S root
 popd
