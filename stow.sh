8b6c7d821a33c07c7fccf852aa86f59c0e87ae5b
wkuinkc <wkuinkc@gmail.com>
2019-03-16 22:57:01 +0000

    Update dotfiles

--- stow.sh
+++ stow.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "$1" = "" ]
-then
-  echo "usage: stow.sh target-directory" >&2
-  exit 1
-fi
-TARGET="$1"
-pushd "$DIR"
-stow -t "$TARGET"/ -S root
-popd
+
+cd "$DIR"
+stow --target "${1:-$HOME}" home
