e318e82ef7a0849651b4628fde5ea0e572540a71
wkuinkc <wkuinkc@gmail.com>
2019-05-05 05:40:23 +0000

    Cleanup dependencies file also

--- dependencies.sh
+++ dependencies.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -euo pipefail
-DIR="$(cd "$(dirname "$0")" && pwd)"
+
+DIR=$(cd $(dirname $0) && pwd)
 cd "$DIR"
 
 if ! hash stow 2>/dev/null; then
   echo "Installing GNU stow to symlink files..."
+
   if hash apt-get 2>/dev/null; then
-    echo sudo apt-get update && sudo apt-get install -y stow
-    sudo apt-get update && sudo apt-get install -y stow
+    sudo apt-get update
+    sudo apt-get install -y stow
+    exit 0
   elif hash yum 2>/dev/null; then
-    echo sudo yum install -y stow --enablerepo=epel
     sudo yum install -y stow --enablerepo=epel
-  else
-    echo "Neither apt-get nor yum found. Terminating..."
-    exit 1
+    exit 0
   fi
+
+  echo "Neither apt-get nor yum found. Terminating..."
+  exit 1
 fi
