70fe17742c5d2725cc797a300718a20d10205ac2
Bill Underwood <wkuinkc@gmail.com>
2021-10-24 00:13:07 +0000

    changed dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-DIR=$(cd $(dirname $0) && pwd)
-cd "$DIR"
+echo "Installing git and GNU stow..."
 
-if ! hash stow 2>/dev/null; then
-  echo "Installing GNU stow to symlink files..."
-
-  if hash apt-get 2>/dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y stow
-    exit 0
-  elif hash yum 2>/dev/null; then
-    sudo yum install -y stow --enablerepo=epel
-    exit 0
-  fi
-
+if hash apt-get 2>/dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y stow
+  exit 0
+elif hash yum 2>/dev/null; then
+  sudo yum install -y stow --enablerepo=epel
+  exit 0
+else
   echo "Neither apt-get nor yum found. Terminating..."
   exit 1
 fi
